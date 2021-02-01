da0b95bdb46e3bab9713bad54edb453f8887b7da
Bruno Lima <brunothelima@gmail.com>
2021-02-01 01:24:24 -0300

    Better documentation for glob2 functions

--- src/pages/Nav.vue
+++ src/pages/Nav.vue
@@ -28,18 +28,20 @@
 
 <template>
   <nav>
-    <header><Settings /></header>
-    <ul>
-      <li><router-link to="colors">{{ t('colors') }}</router-link></li>
-      <li><router-link to="buttons">{{ t('buttons') }}</router-link></li>
-      <li><router-link to="icons">{{ t('icons') }}</router-link></li>
-      <li><router-link to="text">{{ t('text') }}</router-link></li>
-      <li><router-link to="dialogs">{{ t('dialogs') }}</router-link></li>
-      <li><router-link to="form">{{ t('form') }}</router-link></li>
-      <li><router-link to="editor">{{ t('editor') }}</router-link></li>
-      <li><router-link to="images">{{ t('images') }}</router-link></li>
-      <li><router-link to="tests">{{ t('tests') }}</router-link></li>
-    </ul>
+    <header>
+      <Settings />
+    </header>
+    <div>
+      <router-link to="colors">{{ t('colors') }}</router-link>
+      <router-link to="buttons">{{ t('buttons') }}</router-link>
+      <router-link to="icons">{{ t('icons') }}</router-link>
+      <router-link to="text">{{ t('text') }}</router-link>
+      <router-link to="dialogs">{{ t('dialogs') }}</router-link>
+      <router-link to="form">{{ t('form') }}</router-link>
+      <router-link to="editor">{{ t('editor') }}</router-link>
+      <router-link to="images">{{ t('images') }}</router-link>
+      <router-link to="tests">{{ t('tests') }}</router-link>
+    </div>
   </nav>
 </template>
 
@@ -47,9 +49,9 @@
 import { defineAsyncComponent } from 'vue'
 import { useI18n } from '@src/composables'
 
-const  Settings = defineAsyncComponent(
+const Settings = defineAsyncComponent(
   () => import('@src/components/Settings.vue')
-) 
+)
 
 const { t } = useI18n()
 </script>
@@ -63,43 +65,46 @@ nav {
   background: var(--color-x-11);
   border-bottom-right-radius: var(--input-border-radius);
   box-shadow: var(--box-shadow-light);
-  border-right: var(--input-border-width) var(--input-border-style) var(--color-a-4);
-  border-bottom: var(--input-border-width) var(--input-border-style) var(--color-a-4);
+  border-right: var(--input-border-width) var(--input-border-style)
+    var(--color-a-4);
+  border-bottom: var(--input-border-width) var(--input-border-style)
+    var(--color-a-4);
   transition: transform 200ms ease;
   @media screen and (max-width: #{$tablet-brakepoint}) {
     position: fixed;
     transform: translateX(-95%);
   }
   &:after {
-    content: '';
+    content: "";
     position: absolute;
-    right: -40px; top: 0;
+    right: -40px;
+    top: 0;
     height: 100%;
     width: 40px;
   }
   &:hover {
     transform: translateX(0);
   }
-  header {
-    padding: 1em;
-    border-bottom: var(--input-border-width) var(--input-border-style) var(--color-a-6);
-    &::v-deep(.input\:select__selected) {
-      padding: 0.5em 48px 0.5em 1em;
-    }
-    &::v-deep(.input\:select__wrapper) {
-      border-color: var(--color-a-4);
-    }
-  }
-  ul li {
-    padding: 1em 2em;
-    border-bottom: var(--input-border-width) var(--input-border-style) var(--color-a-6);
-  }
-  ul li a {
-    display: inline-block;
-    transition: color var(--transition-duration);
-  }
-  ul li a.router-link-exact-active {
-    color: var(--color-x-1);
-  }
+}
+nav header {
+  padding: 1em;
+  border-bottom: var(--input-border-width) var(--input-border-style)
+    var(--color-a-6);
+}
+nav header::v-deep(.input\:select__selected) {
+  padding: 0.5em 48px 0.5em 1em;
+}
+nav header::v-deep(.input\:select__wrapper) {
+  border-color: var(--color-a-4);
+}
+div a {
+  display: flex;
+  padding: 1em 2em;
+  border-bottom: var(--input-border-width) var(--input-border-style)
+    var(--color-a-6);
+  transition: color var(--transition-duration);
+}
+div a.router-link-exact-active {
+  color: var(--color-x-1);
 }
 </style>
--- src/utils/glob2.ts
+++ src/utils/glob2.ts
@@ -1,44 +1,94 @@
 import { defineAsyncComponent } from 'vue'
 
-// Creates a .vue component collection, using their file names template tag ref
+/**
+ * This function will return a component collection from the
+ * imported meta glob/globEager lists passed as ...args.
+ * 
+ * For example:
+ * 
+ * const components = glob2Components(
+ *   import.meta.globEager('./pages/static/**.vue),
+ *   import.meta.glob('./examples/async/**.vue),
+ *   ...
+ * )
+ * 
+ * console.log(components)
+ * 
+ * {
+ *   'PageStaticA': PageStaticA,
+ *   'PageStaticB': PageStaticB,
+ *    ...
+ *   'PageAsyncA': Promise<PageAsyncA>,
+ *   'PageAsyncB': Promise<PageAsyncB>
+ *    ...
+ * }
+ *
+ */
 export const glob2Components = (...args: any) => {
-  const components: any = {}
-  let globs = Object.assign({}, ...args)
+  let collection: any = {}
+  // Combining the impot.meta.glob/globEager lists
+  let globs = Object.assign({}, ...args) 
 
   for (let componentPath in globs) {
     let componentName = componentPath.split('/').pop()?.replace('.vue', '') 
 
-    console.log()
-    
-    if (Object.keys(components).includes('index')) {
+    // Gives the folder name to any component found at the folder/index.vue file
+    if (Object.keys(collection).includes('index')) {
       componentName = componentPath.split('/')[componentPath.split('/').length - 2]
     }
-    
-    componentName = componentName?.replace('.vue', '')
+
+    /**
+     * Here we identify the async component and execute its own import function 
+     * before adding it to the component collection
+     */
     if (typeof globs[componentPath] === 'function') {
-      components[`${componentName}`] = defineAsyncComponent(
+      collection[`${componentName}`] = defineAsyncComponent(
         () => globs[componentPath]()
       )
       continue
     }
     
-    components[`${componentName}`] = globs[componentPath].default
+    // Adding the static components to the component collection
+    collection[`${componentName}`] = globs[componentPath].default
   }
 
-  return components
+  return collection
 }
 
 // Creates a route list based on file structure from glob arguments
-export const glob2Routes = (routeIndex: string, ...args: any) => {
-  const routes = []
+/**
+ * This function will return a route array based on the given
+ * glob lists components paths, prefixed by its first argument.
+ * 
+ * For example:
+ * 
+ * const routes = glob2Routes(
+ *   '/super-discount/', 
+ *   import.meta.globEager('./Shoes/**.vue'),
+ *   import.meta.glob('./Pants/**.vue'),
+ * )
+ * 
+ * console.log(routes)
+ * 
+ * [
+ *  { path: '/super-discount/shoes-a', component: ShoesA },
+ *  { path: '/super-discount/shoes-b', component: ShoesB },
+ *  ...
+ *  { path: '/super-discount/pants-a', component: Promise<PantsA> },
+ *  { path: '/super-discount/pants-b', component: Promise<PantsB> },
+ *  ...
+ * ]
+ */
+export const glob2Routes = (routePrefix: string, ...args: any) => {
   const components = glob2Components(...args)
+  const routes = []
 
   for (let componentName in components) {    
-    let routePath = componentName.replace(/([a-z])([A-Z])/g, '$1 $2')
-    routePath = routePath.replace(' ', '-').toLowerCase()
+    let routeSufix = componentName.replace(/([a-z])([A-Z])/g, '$1 $2')
+    routeSufix = routeSufix.replace(' ', '-').toLowerCase()
 
     routes.push({
-      path: `${routeIndex}/${routePath}`,
+      path: `${routePrefix}/${routeSufix}`,
       component: components[componentName]
     })
   }
