7fafd74e04255f5b2126f29dd92b0d4658a499a7
Bruno Lima <brunothelima@gmail.com>
2021-01-31 23:00:58 -0300

    Typing fix using Volar instead of Vetur & Refactor complete

--- src/pages/Playground/Text/Examples/Paragraph.vue
+++ src/pages/Playground/Text/Examples/Paragraph.vue
@@ -14,8 +14,11 @@
     <h6>{{ t('title') }}:</h6>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      <a href="/">Impedit nisi cupiditate sunt</a> fugiat reiciendis dicta consequatur
-      <b>assumenda temporibus enim officia</b>, unde dolorem quibusdam <i>reprehenderit odit voluptas magni</i> corrupti
+      <a
+        href="/"
+      >Impedit nisi cupiditate sunt</a> fugiat reiciendis dicta consequatur
+      <b>assumenda temporibus enim officia</b>, unde dolorem quibusdam
+      <i>reprehenderit odit voluptas magni</i> corrupti
       id nulla? lorem ipsum dolor sit amet consectetur adipisicing elit.
     </p>
   </div>
